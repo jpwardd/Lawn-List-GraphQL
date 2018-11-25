d14eeb190259d8ddeb56fa31b9122a18bad52f22
jpwardd <jward9421@gmail.com>
2018-11-24 20:51:46 -0500

    added react router v4 and finished linking up apollo client

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -6,4 +6,10 @@ Types::QueryType = GraphQL::ObjectType.define do
     # resolve would be called in order to fetch data for that field
     resolve -> (obj, args, ctx) { Customer.all }
   end
+
+  field :customer, Types::CustomerType do
+    description 'Retreive a customer by ID'
+    argument :id, !types.ID, 'The ID of the customer to retreive'
+    resolve ->(obj, args, ctx) { Customer.find(args[:id]) }
+  end
 end
--- app/javascript/react/components/Nav.js
+++ app/javascript/react/components/Nav.js
@@ -9,7 +9,7 @@ import MenuIcon from "@material-ui/icons/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
 import Menu from "@material-ui/core/Menu";
 import Button from "@material-ui/core/Button"
-import { Link } from "react-router"
+import { Link } from "react-router-dom"
 
 
 
